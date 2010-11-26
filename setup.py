42c37ff6632998e043fd60191722a9c69e500069
Alfredo Deza <alfredodeza@gmail.com>
2010-11-26 11:13:10 -0500

    frozen dependencies and made templates optional and not required for Pecan

--- setup.py
+++ setup.py
@@ -20,23 +20,20 @@ class PyTest(Command):
 # determine requirements
 #
 requirements = [
-  "WebOb >= 1.0.0", 
-  "WebCore >= 1.0.0",
-  "simplegeneric >= 0.7",
-  "Genshi >= 0.6",
-  "Kajiki >= 0.2.2",
-  "Mako >= 0.3",
-  "py >= 1.3.4",
-  "WebTest >= 1.2.2",
-  "Paste >= 1.7.5.1",
-  "PasteScript >= 1.7.3",
-  "formencode >= 1.2.2"
+  "WebOb == 1.0.0", 
+  "WebCore == 1.0.0",
+  "simplegeneric == 0.7",
+  "py == 1.3.4",
+  "WebTest == 1.2.2",
+  "Paste == 1.7.5.1",
+  "PasteScript == 1.7.3",
+  "formencode == 1.2.2"
 ]
 
 try:
     import json
 except:
-    requirements.append("simplejson >= 2.1.1")
+    requirements.append("simplejson == 2.1.1")
 
 
 #
