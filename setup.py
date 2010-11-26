3dc4d292878a53f32662e5be5d4e57a839313db8
Alfredo Deza <alfredodeza@gmail.com>
2010-11-26 11:16:00 -0500

    moved testing dependencies to tests_require as it is test-specific and not Pecan related.

--- setup.py
+++ setup.py
@@ -5,6 +5,11 @@ version = '0.1'
 #
 # integration with py.test for `python setup.py test`
 #
+tests_require = [
+  "py == 1.3.4",
+  "WebTest == 1.2.2",
+]
+
 class PyTest(Command):
     user_options = []
     def initialize_options(self):
@@ -23,8 +28,6 @@ requirements = [
   "WebOb == 1.0.0", 
   "WebCore == 1.0.0",
   "simplegeneric == 0.7",
-  "py == 1.3.4",
-  "WebTest == 1.2.2",
   "Paste == 1.7.5.1",
   "PasteScript == 1.7.3",
   "formencode == 1.2.2"
