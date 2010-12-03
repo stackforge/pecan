35f21d37131b7b9c63d7b4de7ebbe6b5b7c40000
Mark McClain <mark.mcclain@shootq.com>
2010-12-03 15:20:08 -0500

    fixed import regression

--- pecan/configuration.py
+++ pecan/configuration.py
@@ -19,15 +19,20 @@ def import_runtime_conf(conf):
         parts = conf.split('.')
         name = '.'.join(parts[:-1])
         fromlist = parts[-1:]
-    
+
+        try:
+            module = __import__(name, fromlist=fromlist)
+            conf_mod = getattr(module, parts[-1])
+        except ImportError, e:
+            raise ImportError('No module named %s' % conf)
+        except AttributeError, e:
+            raise ImportError('No module named %s' % conf)
     else:
         name = conf
-        fromlist = []
 
-    try:
-        module = __import__(name, fromlist=fromlist)
-        return getattr(module, parts[-1])
-    except ImportError, e:
-        raise ImportError('No module named %s' % conf)
-    except AttributeError, e:
-        raise ImportError('No module named %s' % conf)
+        try:
+            conf_mod =  __import__(name)
+        except ImportError, e:
+            raise ImportError('No module named %s' % conf)
+
+    return conf_mod
