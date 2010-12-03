5c80bf3c699652b450e84fdd44a7f02944dd51a7
Alfredo Deza <alfredodeza@gmail.com>
2010-12-03 10:45:27 -0500

    re-did the development configuration template to accomodate for new changes

--- templates/project/+egg+/config/development.py
+++ templates/project/+egg+/config/development.py
@@ -1,7 +1,14 @@
-from pecan.configuration import PecanConfig
+from ${egg}.controllers.root import RootController
+from pecan.configuration import server, application 
 
-class Development(PecanConfig):
-    port    = '8080'
-    host    = '0.0.0.0'
-    threads = 1
+
+# Server Specific Configurations
+server.port = '8080'
+server.host = '0.0.0.0'
+
+# Pecan Application Configurations
+application.root = RootController()
+application.static_root = 'public' 
+application.template_path = 'test_conf/templates'
+application.debug = True 
 
