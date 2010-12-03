3232968495d49293fd6a0b7ea1a30e067571ddcc
Alfredo Deza <alfredodeza@gmail.com>
2010-12-03 10:47:37 -0500

    removed test.py since once config file should be enough as an example

--- templates/project/+egg+/config/test.py
+++ templates/project/+egg+/config/test.py
@@ -1,5 +1,15 @@
-from pecan.configuration import PecanConfig
+from ${egg}.controllers.root import RootController
+from pecan.configuration import server, application 
 
-class Test(PecanConfig):
-    pass
+
+# Server Specific Configurations
+server.port = '8080'
+server.host = '0.0.0.0'
+
+# Pecan Application Configurations
+application.root = RootController()
+application.static_root = 'public' 
+application.template_path = 'test_conf/templates'
+application.debug = True 
+
 
