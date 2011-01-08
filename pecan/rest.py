d2b8fb946295a54c1add170df9b8e0560ee62537
Yoann Roman <yoann@shootq.com>
2011-01-07 20:18:08 -0500

    Filling out functionality for RestController and first set of tests for it

--- pecan/rest.py
+++ pecan/rest.py
@@ -1,43 +1,161 @@
+from inspect import getargspec, ismethod
+
+from core import abort, request
 from decorators import expose
-from core import request
+from routing import iscontroller, lookup_controller
 
 
 class RestController(object):
-    # TODO: implement the following:
-    #   - get, new, edit, post_delete, get_delete
-    #   - see: http://turbogears.org/2.1/docs/modules/tgcontroller.html
     
     @expose()
     def _route(self, args):
-        method = request.GET.get('_method', request.method)
         
-        if method == 'GET':
-            if len(args):
-                return self.get_one, args
-            return self.get_all, []
-        elif method == 'POST':
-            return self.post, []
-        elif method == 'PUT' and len(args):
-            return self.put, args
-        elif method == 'DELETE' and len(args):
-            return self.delete, args
+        # convention uses "_method" to handle browser-unsupported methods
+        method = request.GET.get('_method', request.method).lower()
+        
+        # make sure DELETE/PUT requests don't use GET
+        if request.method == 'GET' and method in ('delete', 'put'):
+            abort(405)
+        
+        # check for nested controllers
+        result = self._find_sub_controllers(args)
+        if result:
+            return result
+        
+        # handle the request
+        handler = getattr(self, '_handle_%s' % method, self._handle_custom)
+        result = handler(method, args)
+        
+        # remove the "_method" workaround
+        if '_method' in request.POST:
+            del request.POST['_method']
+        if '_method' in request.GET:
+            del request.GET['_method']
+        
+        # return the result
+        return result
     
-    @expose()
-    def get_one(self, id):
-        raise NotImplemented
+    def _find_controller(self, *args):
+        for name in args:
+            obj = getattr(self, name, None)
+            if obj and iscontroller(obj):
+                return obj
+        return None
     
-    @expose()
-    def get_all(self):
-        raise NotImplemented
+    def _find_sub_controllers(self, remainder):
+        
+        # need either a get_one or get to parse args
+        method = None
+        for name in ('get_one', 'get'):
+            if hasattr(self, name):
+                method = name
+                break
+        if not method:
+            return
+        
+        # get the args to figure out how much to chop off
+        args = getargspec(getattr(self, method))
+        fixed_args = len(args[0][1:])
+        var_args = args[1]
+        
+        # attempt to locate a sub-controller
+        if var_args:
+            for i, item in enumerate(remainder):
+                controller = getattr(self, item, None)
+                if controller and not ismethod(controller):
+                    request.context.setdefault('routing_args', []).extend(remainder[:i])
+                    return lookup_controller(controller, remainder[i + 1:])
+        elif fixed_args < len(remainder) and hasattr(self, remainder[fixed_args]):
+            controller = getattr(self, remainder[fixed_args])
+            if not ismethod(controller):
+                request.context.setdefault('routing_args', []).extend(remainder[:fixed_args])
+                return lookup_controller(controller, remainder[fixed_args + 1:])
     
-    @expose()
-    def post(self):
-        raise NotImplemented
+    def _handle_custom(self, method, remainder):
+        
+        # try finding a post_{custom} or {custom} method first
+        controller = self._find_controller('post_%s' % method, method)
+        if controller:
+            return controller, remainder
+        
+        # if no controller exists, try routing to a sub-controller; note that 
+        # since this isn't a safe GET verb, any local exposes are 405'd
+        if remainder:
+            if self._find_controller(remainder[0]):
+                abort(405)
+            sub_controller = getattr(self, remainder[0], None)
+            if sub_controller:
+                return lookup_controller(sub_controller, remainder[1:])
+        
+        abort(404)
     
-    @expose()
-    def put(self, id):
-        raise NotImplemented
+    def _handle_get(self, method, remainder):
+        
+        # route to a get_all or get if no additional parts are available
+        if not remainder:
+            controller = self._find_controller('get_all', 'get')
+            if controller:
+                return controller, []
+            abort(404)
+        
+        # check for new/edit/delete GET requests
+        method_name = remainder[-1]
+        if method_name in ('new', 'edit', 'delete'):
+            if method_name == 'delete':
+                method_name = 'get_delete'
+            controller = self._find_controller(method_name)
+            if controller:
+                return controller, remainder[:-1]
+        
+        # check for custom GET requests
+        if method_name in getattr(self, '_custom_actions', []):
+            controller = self._find_controller(method_name, 'get_%s' % method_name)
+            if controller:
+                return controller, remainder[:-1]
+        controller = getattr(self, remainder[0], None)
+        if controller and not ismethod(controller):
+            return controller, remainder[1:]
+        
+        # finally, check for the regular get_one/get requests
+        controller = self._find_controller('get_one', 'get')
+        if controller:
+            return controller, remainder
+        
+        abort(404)
     
-    @expose()
-    def delete(self, id):
-        raise NotImplemented
+    def _handle_delete(self, method, remainder):
+        
+        # check for post_delete/delete requests first
+        controller = self._find_controller('post_delete', 'delete')
+        if controller:
+            return controller, remainder
+        
+        # if no controller exists, try routing to a sub-controller; note that 
+        # since this is a DELETE verb, any local exposes are 405'd
+        if remainder:
+            if self._find_controller(remainder[0]):
+                abort(405)
+            sub_controller = getattr(self, remainder[0], None)
+            if sub_controller:
+                return lookup_controller(sub_controller, remainder[1:])
+        
+        abort(404)
+    
+    def _handle_post(self, method, remainder):
+        
+        # check for custom controllers or sub-controllers
+        if remainder:
+            controller = self._find_controller(remainder[0])
+            if controller:
+                return controller, remainder[1:]
+            sub_controller = getattr(self, remainder[0], None)
+            if sub_controller and not ismethod(sub_controller):
+                return lookup_controller(sub_controller, remainder[1:])
+        
+        # check for regular post/put requests
+        controller = self._find_controller(method)
+        if controller:
+            return controller, remainder
+        abort(404)
+    
+    _handle_put = _handle_post
--- tests/test_rest.py
+++ tests/test_rest.py
@@ -12,26 +12,38 @@ class TestRestController(object):
         class ThingsController(RestController):
             data = ['zero', 'one', 'two', 'three']
             
-            @expose('json')
+            @expose()
             def get_one(self, id):
                 return self.data[int(id)]
-
+            
             @expose('json')
             def get_all(self):
                 return dict(items=self.data)
-                
-            @expose('json')
+            
+            @expose()
+            def new(self):
+                return 'NEW'
+            
+            @expose()
             def post(self, value):
                 self.data.append(value)
                 response.status = 302
                 return 'CREATED'
-
-            @expose('json')
+            
+            @expose()
+            def edit(self, id):
+                return 'EDIT %s' % self.data[int(id)]
+            
+            @expose()
             def put(self, id, value):
                 self.data[int(id)] = value
                 return 'UPDATED'
             
-            @expose('json')
+            @expose()
+            def get_delete(self, id):
+                return 'DELETE %s' % self.data[int(id)]
+            
+            @expose()
             def delete(self, id):
                 del self.data[int(id)]
                 return 'DELETED'
@@ -51,44 +63,354 @@ class TestRestController(object):
         for i, value in enumerate(ThingsController.data):
             r = app.get('/things/%d' % i)
             assert r.status_int == 200
-            assert r.body == dumps(value)
+            assert r.body == value
         
         # test post
         r = app.post('/things', {'value':'four'})
         assert r.status_int == 302
-        assert r.body == dumps('CREATED')
+        assert r.body == 'CREATED'
         
         # make sure it works
         r = app.get('/things/4')
         assert r.status_int == 200
-        assert r.body == dumps('four')
+        assert r.body == 'four'
+        
+        # test edit
+        r = app.get('/things/3/edit')
+        assert r.status_int == 200
+        assert r.body == 'EDIT three'
         
         # test put
         r = app.put('/things/4', {'value':'FOUR'})
         assert r.status_int == 200
-        assert r.body == dumps('UPDATED')
+        assert r.body == 'UPDATED'
         
         # make sure it works
         r = app.get('/things/4')
         assert r.status_int == 200
-        assert r.body == dumps('FOUR')
+        assert r.body == 'FOUR'
+        
+        # test put with _method parameter and GET
+        r = app.get('/things/4?_method=put', {'value':'FOUR!'}, status=405)
+        assert r.status_int == 405
+        
+        # make sure it works
+        r = app.get('/things/4')
+        assert r.status_int == 200
+        assert r.body == 'FOUR'
+        
+        # test put with _method parameter and POST
+        r = app.post('/things/4?_method=put', {'value':'FOUR!'})
+        assert r.status_int == 200
+        assert r.body == 'UPDATED'
+        
+        # make sure it works
+        r = app.get('/things/4')
+        assert r.status_int == 200
+        assert r.body == 'FOUR!'
+        
+        # test get delete
+        r = app.get('/things/4/delete')
+        assert r.status_int == 200
+        assert r.body == 'DELETE FOUR!'
         
         # test delete
         r = app.delete('/things/4')
         assert r.status_int == 200
-        assert r.body == dumps('DELETED')
+        assert r.body == 'DELETED'
         
         # make sure it works
         r = app.get('/things')
         assert r.status_int == 200
         assert len(loads(r.body)['items']) == 4
         
-        # test _method parameter
-        r = app.get('/things/3?_method=DELETE')
+        # test delete with _method parameter and GET
+        r = app.get('/things/3?_method=DELETE', status=405)
+        assert r.status_int == 405
+        
+        # make sure it works
+        r = app.get('/things')
         assert r.status_int == 200
-        assert r.body == dumps('DELETED')
+        assert len(loads(r.body)['items']) == 4
+        
+        # test delete with _method parameter and POST
+        r = app.post('/things/3?_method=DELETE')
+        assert r.status_int == 200
+        assert r.body == 'DELETED'
         
         # make sure it works
         r = app.get('/things')
         assert r.status_int == 200
         assert len(loads(r.body)['items']) == 3
+    
+    def test_nested_rest(self):
+        
+        class BarsController(RestController):
+            
+            data = [['zero-zero', 'zero-one'], ['one-zero', 'one-one']]
+            
+            @expose()
+            def get_one(self, foo_id, id):
+                return self.data[int(foo_id)][int(id)]
+            
+            @expose('json')
+            def get_all(self, foo_id):
+                return dict(items=self.data[int(foo_id)])
+            
+            @expose()
+            def new(self, foo_id):
+                return 'NEW FOR %s' % foo_id
+            
+            @expose()
+            def post(self, foo_id, value):
+                foo_id = int(foo_id)
+                if len(self.data) < foo_id + 1:
+                    self.data.extend([[]] * (foo_id - len(self.data) + 1))
+                self.data[foo_id].append(value)
+                response.status = 302
+                return 'CREATED FOR %s' % foo_id
+            
+            @expose()
+            def edit(self, foo_id, id):
+                return 'EDIT %s' % self.data[int(foo_id)][int(id)]
+            
+            @expose()
+            def put(self, foo_id, id, value):
+                self.data[int(foo_id)][int(id)] = value
+                return 'UPDATED'
+            
+            @expose()
+            def get_delete(self, foo_id, id):
+                return 'DELETE %s' % self.data[int(foo_id)][int(id)]
+            
+            @expose()
+            def delete(self, foo_id, id):
+                del self.data[int(foo_id)][int(id)]
+                return 'DELETED'
+        
+        class FoosController(RestController):
+            
+            data = ['zero', 'one']
+            
+            bars = BarsController()
+            
+            @expose()
+            def get_one(self, id):
+                return self.data[int(id)]
+
+            @expose('json')
+            def get_all(self):
+                return dict(items=self.data)
+            
+            @expose()
+            def new(self):
+                return 'NEW'
+            
+            @expose()
+            def edit(self, id):
+                return 'EDIT %s' % self.data[int(id)]
+            
+            @expose()
+            def post(self, value):
+                self.data.append(value)
+                response.status = 302
+                return 'CREATED'
+
+            @expose()
+            def put(self, id, value):
+                self.data[int(id)] = value
+                return 'UPDATED'
+            
+            @expose()
+            def get_delete(self, id):
+                return 'DELETE %s' % self.data[int(id)]
+            
+            @expose()
+            def delete(self, id):
+                del self.data[int(id)]
+                return 'DELETED'
+        
+        class RootController(object):
+            foos = FoosController()
+        
+        # create the app
+        app = TestApp(make_app(RootController()))
+        
+        # test get_all
+        r = app.get('/foos')
+        assert r.status_int == 200
+        assert r.body == dumps(dict(items=FoosController.data))
+        
+        # test nested get_all
+        r = app.get('/foos/1/bars')
+        assert r.status_int == 200
+        assert r.body == dumps(dict(items=BarsController.data[1]))
+        
+        # test get_one
+        for i, value in enumerate(FoosController.data):
+            r = app.get('/foos/%d' % i)
+            assert r.status_int == 200
+            assert r.body == value
+        
+        # test nested get_one
+        for i, value in enumerate(FoosController.data):
+            for j, value in enumerate(BarsController.data[i]):
+                r = app.get('/foos/%s/bars/%s' % (i, j))
+                assert r.status_int == 200
+                assert r.body == value
+        
+        # test post
+        r = app.post('/foos', {'value':'two'})
+        assert r.status_int == 302
+        assert r.body == 'CREATED'
+        
+        # make sure it works
+        r = app.get('/foos/2')
+        assert r.status_int == 200
+        assert r.body == 'two'
+        
+        # test nested post
+        r = app.post('/foos/2/bars', {'value':'two-zero'})
+        assert r.status_int == 302
+        assert r.body == 'CREATED FOR 2'
+        
+        # make sure it works
+        r = app.get('/foos/2/bars/0')
+        assert r.status_int == 200
+        assert r.body == 'two-zero'
+        
+        # test edit
+        r = app.get('/foos/1/edit')
+        assert r.status_int == 200
+        assert r.body == 'EDIT one'
+        
+        # test nested edit
+        r = app.get('/foos/1/bars/1/edit')
+        assert r.status_int == 200
+        assert r.body == 'EDIT one-one'
+        
+        # test put
+        r = app.put('/foos/2', {'value':'TWO'})
+        assert r.status_int == 200
+        assert r.body == 'UPDATED'
+        
+        # make sure it works
+        r = app.get('/foos/2')
+        assert r.status_int == 200
+        assert r.body == 'TWO'
+        
+        # test nested put
+        r = app.put('/foos/2/bars/0', {'value':'TWO-ZERO'})
+        assert r.status_int == 200
+        assert r.body == 'UPDATED'
+        
+        # make sure it works
+        r = app.get('/foos/2/bars/0')
+        assert r.status_int == 200
+        assert r.body == 'TWO-ZERO'
+        
+        # test put with _method parameter and GET
+        r = app.get('/foos/2?_method=put', {'value':'TWO!'}, status=405)
+        assert r.status_int == 405
+        
+        # make sure it works
+        r = app.get('/foos/2')
+        assert r.status_int == 200
+        assert r.body == 'TWO'
+        
+        # test nested put with _method parameter and GET
+        r = app.get('/foos/2/bars/0?_method=put', {'value':'ZERO-TWO!'}, status=405)
+        assert r.status_int == 405
+        
+        # make sure it works
+        r = app.get('/foos/2/bars/0')
+        assert r.status_int == 200
+        assert r.body == 'TWO-ZERO'
+        
+        # test put with _method parameter and POST
+        r = app.post('/foos/2?_method=put', {'value':'TWO!'})
+        assert r.status_int == 200
+        assert r.body == 'UPDATED'
+        
+        # make sure it works
+        r = app.get('/foos/2')
+        assert r.status_int == 200
+        assert r.body == 'TWO!'
+        
+        # test nested put with _method parameter and POST
+        r = app.post('/foos/2/bars/0?_method=put', {'value':'TWO-ZERO!'})
+        assert r.status_int == 200
+        assert r.body == 'UPDATED'
+        
+        # make sure it works
+        r = app.get('/foos/2/bars/0')
+        assert r.status_int == 200
+        assert r.body == 'TWO-ZERO!'
+        
+        # test get delete
+        r = app.get('/foos/2/delete')
+        assert r.status_int == 200
+        assert r.body == 'DELETE TWO!'
+        
+        # test nested get delete
+        r = app.get('/foos/2/bars/0/delete')
+        assert r.status_int == 200
+        assert r.body == 'DELETE TWO-ZERO!'
+        
+        # test nested delete
+        r = app.delete('/foos/2/bars/0')
+        assert r.status_int == 200
+        assert r.body == 'DELETED'
+        
+        # make sure it works
+        r = app.get('/foos/2/bars')
+        assert r.status_int == 200
+        assert len(loads(r.body)['items']) == 0
+        
+        # test delete
+        r = app.delete('/foos/2')
+        assert r.status_int == 200
+        assert r.body == 'DELETED'
+        
+        # make sure it works
+        r = app.get('/foos')
+        assert r.status_int == 200
+        assert len(loads(r.body)['items']) == 2
+        
+        # test nested delete with _method parameter and GET
+        r = app.get('/foos/1/bars/1?_method=DELETE', status=405)
+        assert r.status_int == 405
+        
+        # make sure it works
+        r = app.get('/foos/1/bars')
+        assert r.status_int == 200
+        assert len(loads(r.body)['items']) == 2
+        
+        # test delete with _method parameter and GET
+        r = app.get('/foos/1?_method=DELETE', status=405)
+        assert r.status_int == 405
+        
+        # make sure it works
+        r = app.get('/foos')
+        assert r.status_int == 200
+        assert len(loads(r.body)['items']) == 2
+        
+        # test nested delete with _method parameter and POST
+        r = app.post('/foos/1/bars/1?_method=DELETE')
+        assert r.status_int == 200
+        assert r.body == 'DELETED'
+        
+        # make sure it works
+        r = app.get('/foos/1/bars')
+        assert r.status_int == 200
+        assert len(loads(r.body)['items']) == 1
+        
+        # test delete with _method parameter and POST
+        r = app.post('/foos/1?_method=DELETE')
+        assert r.status_int == 200
+        assert r.body == 'DELETED'
+        
+        # make sure it works
+        r = app.get('/foos')
+        assert r.status_int == 200
+        assert len(loads(r.body)['items']) == 1
