5181fd7edccb230b6d292c9ff0d56f59fc19e465
Mark McClain <mark.mcclain@shootq.com>
2011-01-17 12:37:22 -0500

    SQLAlchemy now optional, better jsonify test coverage

--- tests/test_jsonify.py
+++ tests/test_jsonify.py
@@ -1,8 +1,12 @@
-from pecan.jsonify  import jsonify, encode
-from pecan          import Pecan, expose
-from webtest        import TestApp
-from json           import loads
+from datetime           import datetime, date
+from decimal            import Decimal
+from json               import loads
+from unittest           import TestCase
 
+from pecan.jsonify      import jsonify, encode
+from pecan              import Pecan, expose, request
+from webtest            import TestApp
+from webob.multidict    import MultiDict, UnicodeMultiDict
 
 def make_person():
     class Person(object):
@@ -59,3 +63,43 @@ class TestJsonify(object):
         r = app.get('/')
         assert r.status_int == 200
         assert loads(r.body) == {'name':'Jonathan LaCour'}
+
+class TestJsonifyGenericEncoder(TestCase):
+    def test_json_callable(self):
+        class JsonCallable(object):
+            def __init__(self, arg):
+                self.arg = arg
+            def __json__(self):
+                return {"arg":self.arg}
+
+        result = encode(JsonCallable('foo'))
+        assert loads(result) == {'arg':'foo'}
+
+    def test_datetime(self):
+        today = date.today()
+        now = datetime.now()
+
+        result = encode(today)
+        assert loads(result) == str(today)
+
+        result = encode(now)
+        assert loads(result) == str(now)
+
+    def test_decimal(self):
+        # XXX Testing for float match which is inexact
+
+        d = Decimal('1.1')
+        result = encode(d)
+        assert loads(result) == float(d)
+
+    def test_multidict(self):
+        md = MultiDict()
+        md.add('arg', 'foo')
+        md.add('arg', 'bar')
+        result = encode(md)
+        assert loads(result) == {'arg': ['foo', 'bar']}
+
+    def test_fallback_to_builtin_encoder(self):
+        class Foo(object): pass
+
+        self.assertRaises(TypeError, encode, Foo())
