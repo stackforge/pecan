ad04aca7e7cfc7eca0d4e88f19f80dc20a467c27
Mark McClain <mark.mcclain@shootq.com>
2011-06-30 18:59:19 -0400

    adding support for decorating a class with transactional

--- pecan/decorators.py
+++ pecan/decorators.py
@@ -1,4 +1,4 @@
-from inspect import getargspec
+from inspect import getargspec, getmembers, isclass, ismethod
 from util import _cfg
 
 __all__ = [
@@ -90,15 +90,21 @@ def expose(template        = None,
 def transactional(ignore_redirects=True):
     '''
     If utilizing the :mod:`pecan.hooks` ``TransactionHook``, allows you
-    to flag a controller method as being wrapped in a transaction,
+    to flag a controller method or class as being wrapped in a transaction,
     regardless of HTTP method.
     
     :param ignore_redirects: Indicates if the hook should ignore redirects for this controller or not.
     '''
-    
+
     def deco(f):
-        _cfg(f)['transactional'] = True
-        _cfg(f)['transactional_ignore_redirects'] = ignore_redirects
+        if isclass(f):
+            for method in [m[1] for m in getmembers(f) if ismethod(m[1])]:
+                if getattr(method, 'exposed', False):
+                    _cfg(method)['transactional'] = True
+                    _cfg(method)['transactional_ignore_redirects'] = _cfg(method).get('transactional_ignore_redirects', ignore_redirects)
+        else:
+            _cfg(f)['transactional'] = True
+            _cfg(f)['transactional_ignore_redirects'] = ignore_redirects
         return f
     return deco
 
--- tests/test_hooks.py
+++ tests/test_hooks.py
@@ -852,6 +852,208 @@ class TestTransactionHook(object):
         assert run_hook[3] == 'rollback'
         assert run_hook[4] == 'clear'
 
+    def test_transaction_hook_with_transactional_class_decorator(self):
+        run_hook = []
+        
+        @transactional()
+        class RootController(object):
+            @expose()
+            def index(self):
+                run_hook.append('inside')
+                return 'Hello, World!'
+
+            @expose()
+            def redirect(self):
+                redirect('/')
+                
+            @expose()
+            @transactional(False)
+            def redirect_rollback(self):
+                redirect('/')
+
+            @expose()
+            def error(self):
+                return [][1]
+
+            @expose(generic=True)
+            def generic(self):
+                pass
+
+            @generic.when(method='GET')
+            def generic_get(self):
+                run_hook.append('inside')
+                return 'generic get'
+
+            @generic.when(method='POST')
+            def generic_post(self):
+                run_hook.append('inside')
+                return 'generic post'
+                
+        def gen(event):
+            return lambda: run_hook.append(event)
+
+        app = TestApp(make_app(RootController(), hooks=[
+            TransactionHook(
+                start    = gen('start'),
+                start_ro = gen('start_ro'),
+                commit   = gen('commit'),
+                rollback = gen('rollback'),
+                clear    = gen('clear')
+            )
+        ]))
+
+        response = app.get('/')
+        assert response.status_int == 200
+        assert response.body == 'Hello, World!'
+
+        assert len(run_hook) == 6
+        assert run_hook[0] == 'start_ro'
+        assert run_hook[1] == 'clear'
+        assert run_hook[2] == 'start'
+        assert run_hook[3] == 'inside'
+        assert run_hook[4] == 'commit'
+        assert run_hook[5] == 'clear'
+
+        run_hook = []
+
+        # test hooks for /
+
+        response = app.post('/')
+        assert response.status_int == 200
+        assert response.body == 'Hello, World!'
+
+        assert len(run_hook) == 4
+        assert run_hook[0] == 'start'
+        assert run_hook[1] == 'inside'
+        assert run_hook[2] == 'commit'
+        assert run_hook[3] == 'clear'
+
+        #
+        # test hooks for GET /redirect
+        # This controller should always be transactional,
+        # even in the case of redirects
+        #
+        
+        run_hook = []
+        response = app.get('/redirect')
+        assert response.status_int == 302
+        assert len(run_hook) == 5
+        assert run_hook[0] == 'start_ro'
+        assert run_hook[1] == 'clear'        
+        assert run_hook[2] == 'start'
+        assert run_hook[3] == 'commit'
+        assert run_hook[4] == 'clear'
+        
+        #
+        # test hooks for POST /redirect
+        # This controller should always be transactional,
+        # even in the case of redirects
+        #
+        
+        run_hook = []
+        
+        response = app.post('/redirect')
+        assert response.status_int == 302
+        assert len(run_hook) == 3
+        assert run_hook[0] == 'start'
+        assert run_hook[1] == 'commit'
+        assert run_hook[2] == 'clear'
+        
+        #
+        # test hooks for GET /redirect_rollback
+        # This controller should always be transactional,
+        # *except* in the case of redirects
+        #
+        run_hook = []
+        
+        response = app.get('/redirect_rollback')
+        assert response.status_int == 302
+        assert len(run_hook) == 5
+        assert run_hook[0] == 'start_ro'
+        assert run_hook[1] == 'clear'        
+        assert run_hook[2] == 'start'        
+        assert run_hook[3] == 'rollback'
+        assert run_hook[4] == 'clear'
+        
+        #
+        # test hooks for POST /redirect_rollback
+        # This controller should always be transactional,
+        # *except* in the case of redirects
+        #
+        
+        run_hook = []
+        
+        response = app.post('/redirect_rollback')
+        assert response.status_int == 302
+        assert len(run_hook) == 3
+        assert run_hook[0] == 'start'
+        assert run_hook[1] == 'rollback'
+        assert run_hook[2] == 'clear'
+        
+        #
+        # Exceptions (other than HTTPFound) should *always*
+        # rollback no matter what
+        #
+        run_hook = []        
+        
+        try:
+            response = app.post('/error')
+        except IndexError:
+            pass
+
+        assert len(run_hook) == 3
+        assert run_hook[0] == 'start'
+        assert run_hook[1] == 'rollback'
+        assert run_hook[2] == 'clear'
+        
+        run_hook = []        
+        
+        try:
+            response = app.get('/error')
+        except IndexError:
+            pass
+
+        assert len(run_hook) == 5
+        assert run_hook[0] == 'start_ro'
+        assert run_hook[1] == 'clear'
+        assert run_hook[2] == 'start'
+        assert run_hook[3] == 'rollback'
+        assert run_hook[4] == 'clear'
+
+        #
+        # test hooks for GET /generic
+        # This controller should always be transactional,
+        #
+        
+        run_hook = []
+        
+        response = app.get('/generic')
+        assert response.status_int == 200
+        assert response.body == 'generic get'
+        assert len(run_hook) == 6
+        assert run_hook[0] == 'start_ro'
+        assert run_hook[1] == 'clear'
+        assert run_hook[2] == 'start'
+        assert run_hook[3] == 'inside'
+        assert run_hook[4] == 'commit'
+        assert run_hook[5] == 'clear'
+        
+        #
+        # test hooks for POST /generic
+        # This controller should always be transactional,
+        #
+        
+        run_hook = []
+        
+        response = app.post('/generic')
+        assert response.status_int == 200
+        assert response.body == 'generic post'
+        assert len(run_hook) == 4
+        assert run_hook[0] == 'start'
+        assert run_hook[1] == 'inside'
+        assert run_hook[2] == 'commit'
+        assert run_hook[3] == 'clear'
+        
 
 class TestRequestViewerHook(object):
     
