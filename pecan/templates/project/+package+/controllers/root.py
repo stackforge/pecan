378fc9317b9122c8b2cca33f4095cb9510842e08
Ryan Petrello <lists@ryanpetrello.com>
2012-03-08 13:43:15 -0500

    Bringing the scaffolded project's test coverage to 100%.

--- pecan/templates/project/+package+/controllers/root.py
+++ pecan/templates/project/+package+/controllers/root.py
@@ -29,7 +29,7 @@ class RootController(object):
     def error(self, status):
         try:
             status = int(status)
-        except ValueError:
-            status = 0
+        except ValueError: # pragma: no cover
+            status = 500
         message = getattr(status_map.get(status), 'explanation', '')
         return dict(status=status, message=message)
