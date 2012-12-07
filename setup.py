338cb6ddcaefee1d390a1d7ec3b01c1bd2438d22
Ryan Petrello <lists@ryanpetrello.com>
2012-12-07 00:23:38 -0500

    Version update.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import sys
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 
-version = '0.2.0a'
+version = '0.2.0b'
 
 #
 # determine requirements
