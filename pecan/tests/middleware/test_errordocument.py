b5d69b5624f3e359c3c0c898e2b2dd23f85af01b
Ryan Petrello <lists@ryanpetrello.com>
2013-04-09 14:00:16 -0400

    Store exceptions raised by ``abort`` in the WSGI environ.
    This makes it easier to access the original exception later in the request cycle (e.g., other middleware or in custom error handlers).

--- pecan/tests/middleware/test_errordocument.py
+++ pecan/tests/middleware/test_errordocument.py
@@ -1,5 +1,8 @@
+import json
+
 from webtest import TestApp
 
+import pecan
 from pecan.middleware.errordocument import ErrorDocumentMiddleware
 from pecan.middleware.recursive import RecursiveMiddleware
 from pecan.tests import PecanTestCase
@@ -52,3 +55,36 @@ class TestErrorDocumentMiddleware(PecanTestCase):
         assert r.status_int == 404
         assert r.body == ('Error: 404 Not Found.  '
                           '(Error page could not be fetched)')
+
+    def test_original_exception(self):
+
+        class RootController(object):
+
+            @pecan.expose()
+            def index(self):
+                if pecan.request.method != 'POST':
+                    pecan.abort(405, 'You have to POST, dummy!')
+                return 'Hello, World!'
+
+            @pecan.expose('json')
+            def error(self, status):
+                return dict(
+                    status=int(status),
+                    reason=pecan.request.environ[
+                        'pecan.original_exception'
+                    ].detail
+                )
+
+        app = pecan.Pecan(RootController())
+        app = RecursiveMiddleware(ErrorDocumentMiddleware(app, {
+            405: '/error/405'
+        }))
+        app = TestApp(app)
+
+        assert app.post('/').status_int == 200
+        r = app.get('/', expect_errors=405)
+        assert r.status_int == 405
+
+        resp = json.loads(r.body)
+        assert resp['status'] == 405
+        assert resp['reason'] == 'You have to POST, dummy!'
