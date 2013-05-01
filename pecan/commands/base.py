512911a7ccd0a51a4d30d2b693b1e3b25f8acddf
Ryan Petrello <lists@ryanpetrello.com>
2013-05-01 12:20:44 -0400

    Add a docstring.

--- pecan/commands/base.py
+++ pecan/commands/base.py
@@ -108,6 +108,12 @@ class BaseCommandMeta(type):
 
     @property
     def summary(cls):
+        """
+        This is used to populate the --help argument on the command line.
+
+        This provides a default behavior which takes the first sentence of the
+        command's docstring and uses it.
+        """
         return cls.__doc__.strip().splitlines()[0].rstrip('.')
 
 
