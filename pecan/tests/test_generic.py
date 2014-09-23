960d83971c712e98c45c830df84fdc61cb8cd15b
Ryan Petrello <lists@ryanpetrello.com>
2014-09-23 13:30:00 -0400

    Fix a routing bug for generic subcontrollers.
    Change-Id: I3764da98f19f02c1f28a29377c6d24238d869930
    Fixes-bug: 1361728

--- pecan/tests/test_generic.py
+++ pecan/tests/test_generic.py
@@ -60,3 +60,29 @@ class TestGeneric(PecanTestCase):
         r = app.delete('/', expect_errors=True)
         assert r.status_int == 405
         assert r.headers['Allow'] == 'GET, PATCH, POST'
+
+    def test_nested_generic(self):
+
+        class SubSubController(object):
+            @expose(generic=True)
+            def index(self):
+                return 'GET'
+
+            @index.when(method='DELETE', template='json')
+            def do_delete(self, name, *args):
+                return dict(result=name, args=', '.join(args))
+
+        class SubController(object):
+            sub = SubSubController()
+
+        class RootController(object):
+            sub = SubController()
+
+        app = TestApp(Pecan(RootController()))
+        r = app.get('/sub/sub/')
+        assert r.status_int == 200
+        assert r.body == b_('GET')
+
+        r = app.delete('/sub/sub/joe/is/cool')
+        assert r.status_int == 200
+        assert r.body == b_(dumps(dict(result='joe', args='is, cool')))
