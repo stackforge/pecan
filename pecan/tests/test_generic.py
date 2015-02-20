465088213b2d884fe09613aed207eb92ceaeef27
Ryan Petrello <lists@ryanpetrello.com>
2015-02-20 09:39:54 -0500

    Prevent @expose(generic=True) on special methods (_route, _lookup, _default).
    Change-Id: Ie180cc4092ab1457dd97176cc72c563e1d4c561f

--- pecan/tests/test_generic.py
+++ pecan/tests/test_generic.py
@@ -86,3 +86,26 @@ class TestGeneric(PecanTestCase):
         r = app.delete('/sub/sub/joe/is/cool')
         assert r.status_int == 200
         assert r.body == b_(dumps(dict(result='joe', args='is, cool')))
+
+
+class TestGenericWithSpecialMethods(PecanTestCase):
+
+    def test_generics_not_allowed(self):
+
+        class C(object):
+
+            def _default(self):
+                pass
+
+            def _lookup(self):
+                pass
+
+            def _route(self):
+                pass
+
+        for method in (C._default, C._lookup, C._route):
+            self.assertRaises(
+                ValueError,
+                expose(generic=True),
+                getattr(method, '__func__', method)
+            )
